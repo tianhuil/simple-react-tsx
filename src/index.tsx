0b7ce255caaddd0030d4802764a5d4b5f0b2c559
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-03-06 21:08:00 -0800

    tslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./styles/index.scss";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import './styles/index.scss'
 
 ReactDOM.render(
   <React.Fragment>
     <App/>
   </React.Fragment>,
-  document.getElementById("app"),
-);
+  document.getElementById('app'),
+)
 
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept()
 }
